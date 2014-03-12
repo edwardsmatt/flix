239f6f91339a49fb0d0fd44e5d93dffd1d07e9e8
Matthew Edwards <matthew.edwards@dete.qld.gov.au>
2014-03-12 10:07:54 +1000

    Require user sign in for a review. Added list of revies to profile page. Updated specs accordingly

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,18 +9,11 @@ describe "A review" do
 
     expect(review.movie).to eq(movie)
   end
-  
+
   it "with example attributes is valid" do
     review = Review.new(review_attributes)
-    
+
     expect(review.valid?).to be_true
-  end
-
-  it "requires a name" do
-    review = Review.new(name: "")
-
-    expect(review.valid?).to be_false
-    expect(review.errors[:name].any?).to be_true
   end
 
   it "requires a comment" do
@@ -29,10 +22,10 @@ describe "A review" do
     expect(review.valid?).to be_false
     expect(review.errors[:comment].any?).to be_true
   end
-  
+
   it "requires a comment over 3 characters" do
     review = Review.new(comment: "X" * 3)
-    
+
     expect(review.valid?).to be_false
     expect(review.errors[:comment].any?).to be_true
   end
