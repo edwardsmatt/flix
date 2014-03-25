05ccbc8f6088d2545b6c2abe617bc83d1349a0e8
Matthew Edwards <matthew.edwards@dete.qld.gov.au>
2014-03-26 09:55:39 +1000

    [Module 9] Through Associations 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :favorites
+
 
   resource :session
   get 'signin' => 'sessions#new'
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   root "movies#index"
   resources :movies do
     resources :reviews
+    resources :favorites
   end
 end
